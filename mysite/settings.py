f81df2d29b87979e02917b5cbc1134232a7f4356
Mauro Roth <maurogroth@gmail.com>
2023-04-09 23:17:33 -0300

    tercer commit: algunos arreglos simples

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-su!%sh#*rk3-gpd76ha0em0!p1q@!vsg@4y31oa-$6v&ml&rlz'
+#SECRET_KEY = 'django-insecure-su!%sh#*rk3-gpd76ha0em0!p1q@!vsg@4y31oa-$6v&ml&rlz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
